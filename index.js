749495f338966b472d5cc2bce70e231f300f6e1f
salrey <tekdehn@gmail.com>
2021-12-02 17:07:07 -0500

    add index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+function fn(...c) {
+    if (!c.every((a) => Number.isNumber(a)))
+      throw "All arguments must be numbers.";
+    return c.reduce((a, b) => a + b);
+  }
+  
+  fn(1, 2, 3); //> 6
+  fn(10, "B", 20); //> error All arguments must be numbers.
+  
