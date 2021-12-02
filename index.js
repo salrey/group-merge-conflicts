063e9d07f1291693beb5dceadd265f2212464913
Tashawnee Guarriello <tashawneeguarriello@pursuit.org>
2021-12-02 17:16:37 -0500

    Tashawnee's Changes

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-function fn(...c) {
-    if (!c.every((a) => Number.isNumber(a)))
-      throw "All arguments must be numbers.";
-    return c.reduce((a, b) => a + b);
-  }
-  
-  fn(1, 2, 3); //> 6
-  fn(10, "B", 20); //> error All arguments must be numbers.
-  
+function func(...array) {
+  if (!array.every((anything) => Number.isNumber(anything)))
+    throw "All arguments must be numbers!";
+  return array.reduce((a, b) => a + b);
+}
+
+fn(1, 2, 3); //> 6
+fn(10, "B", 20); //> error All arguments must be numbers.
